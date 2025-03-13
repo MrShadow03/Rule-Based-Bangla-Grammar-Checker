e28e6d772901ec7be759699cc1484a309cf79f91
Galib <gj.emon35@gmail.com>
2025-03-13 14:26:31 +0600

    added two new research papers

--- WordMiner-Python/main.py
+++ WordMiner-Python/main.py
@@ -2,10 +2,13 @@ import csv
 
 class PosTagger:
     def __init__(self, dict_path, text_path):
-        self.punctuations = ['.', ',', '!', '?', ';', ':', '"', "'", '(', ')', '[', ']', '{', '}', '<', '>', '/', '\\', '|', '@', '#', '$', '%', '^', '&', '*', '-', '_', '=', '+', '~', '`', '‘', '’', '“', '”', '…']
+        self.punctuations = ['।', '.', ',', '!', '?', ';', ':', '"', "'", '(', ')', '[', ']', '{', '}', '<', '>', '/', '\\', '|', '@', '#', '$', '%', '^', '&', '*', '-', '_', '=', '+', '~', '`', '‘', '’', '“', '”', '…']
+        self.sentence_terminals = ['।', '!', '?']
+        self.bangla_digits = ['০', '১', '২', '৩', '৪', '৫', '৬', '৭', '৮', '৯']
         self.noun_suffixes = self.load_noun_suffixes("./data/bangla-noun-suffixes.csv")
         self.text = self.load_text(text_path)
         self.words = self.text.split(" ")
+        self.striped_words = [self.stipe_noun_suffix(word) for word in self.words]
         self.pos_tags = ["NOUN"]
         self.bangla_dict = {}
         self.load_bangla_dict(dict_path)
@@ -30,7 +33,6 @@ class PosTagger:
                     suffixes.append(row[1].strip())
         return suffixes
             
-
     def get_dict(self):
         return self.bangla_dict
     
@@ -45,27 +47,54 @@ class PosTagger:
             text = text.replace(p, '')
         # Remove multiple spaces keeping only one
         text = ' '.join(text.split())
+
+        #remove bangla digits
+        for digit in self.bangla_digits:
+            text = text.replace(digit, '')
+
         return text
     
     def get_unknown_words(self):
-        for word in self.words:
-            if word not in self.bangla_dict.values():
+        for word in self.striped_words:
+            if word.strip() != "" and word not in self.bangla_dict.values():
                 self.unknown_words.append(word)
         return self.unknown_words
     
     def get_known_words(self):
-        for word in self.words:
+        for word in self.striped_words:
             if word in self.bangla_dict.values():
                 self.known_words.append(word)
         return self.known_words
+    
+    def stipe_noun_suffix(self, word):
+        for suffix in self.noun_suffixes:
+            if word.endswith(suffix) and len(word) > len(suffix):
+                striped_word = word[:-len(suffix)]
+                #create a csv file with the original word and the suffix
+                with open("words_and_suffixes.csv", "a", encoding='utf-8') as file:
+                    writer = csv.writer(file)
+                    writer.writerow([striped_word, suffix, len(word), len(suffix)])
+                return striped_word
+        return word
 
     
 
     
 
-# tagger = PosTagger("./data/bangla_word_huge_dataset.csv", "./sample-text.txt")
-# bangla_dict = tagger.get_dict()
+tagger = PosTagger("./data/bangla_word_huge_dataset.csv", "./sample-text.txt")
+bangla_dict = tagger.get_dict()
 
-# print("Unknown count:", len(tagger.get_unknown_words()))
-# print("Known count:", len(tagger.get_known_words()))
+# save the unknown words to a file
+with open("unknown_words.txt", "w", encoding='utf-8') as file:
+    for word in tagger.get_unknown_words():
+        file.write(word + "\n")
+
+# save the known words to a file
+with open("known_words.txt", "w", encoding='utf-8') as file:
+    for word in tagger.get_known_words():
+        file.write(word + "\n")
+
+
+print("Unknown count:", len(tagger.get_unknown_words()))
+print("Known count:", len(tagger.get_known_words()))
 
